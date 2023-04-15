0eae57aa45cf46ea19c3fc76c952d0d9267f2d36
aiokr <hiaiokr@gmail.com>
2023-04-15 12:58:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="header-title">
+      Tripper Labs
+    </div>
+    <div class="header-items">
+      More…
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  z-index: 1000;
+  width: 100%;
+  height: 54px;
+  max-width: none;
+  border-radius: 0;
+  top: 0px;
+  display: flex;
+  flex-direction: row;
+  position: fixed;
+}
+
+.header-title,
+.header-items {
+  line-height: 58px;
+  text-align: center;
+  vertical-align: middle;
+  margin: 0 12px;
+}
+
+.header-title {
+  font-weight: 600;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <section id="hi8" class="hi8sect grid grid-cols-12 h-screen p-12">
+      <div class="content-area col-span-6">
+
+      </div>
+      <div class="star-img-area col-span-6" style="background-image: url(https://stars.tripper.press/api/himawari8);">
+      </div>
+    </section>
+  </main>
+</template>
+<style scoped>
+.star-img-area {
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.star-img-area img {
+  max-height: 100%;
+  width: auto;
+}
+</style>
